276fa9a185ca0e95336b23e0fb05d8925456cafa
luca Bigoni <luca.bigoni@apparound.com>
2020-06-03 22:02:08 +0200

    uguale a commit precedente

--- app/src/main/java/luca/bigoni/mathematicalquiz/ui/contatSupport/ContatSupportFragment.java
+++ app/src/main/java/luca/bigoni/mathematicalquiz/ui/contatSupport/ContatSupportFragment.java
@@ -1,35 +1,59 @@
-package luca.bigoni.mathematicalquiz.ui.home;
+package luca.bigoni.mathematicalquiz.ui.contatSupport;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 import luca.bigoni.mathematicalquiz.R;
 
-public class HomeFragment extends Fragment {
+@SuppressLint("RestrictedApi")
+public class ContatSupportFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private ContatSupportViewModel ContatSupportViewModel;
+private   View root;
+private FloatingActionButton floatingAction;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        ContatSupportViewModel =
+                ViewModelProviders.of(this).get(ContatSupportViewModel.class);
+        root= inflater.inflate(R.layout.fragment_contat_support, container, false);
+        final TextView textView = root.findViewById(R.id.text_contat_support);
+        ContatSupportViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+         FloatingActionButton floatingAction =(FloatingActionButton) root.findViewById(R.id.SendMail);
+        floatingAction.setVisibility(View.VISIBLE);
+        floatingAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
         return root;
     }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+         floatingAction =(FloatingActionButton) root.findViewById(R.id.SendMail);
+        floatingAction.setVisibility(View.INVISIBLE);
+        //Save the fragment's state here
+    }
+
 }
--- app/src/main/java/luca/bigoni/mathematicalquiz/ui/contatSupport/ContatSupportViewModel.java
+++ app/src/main/java/luca/bigoni/mathematicalquiz/ui/contatSupport/ContatSupportViewModel.java
@@ -1,16 +1,16 @@
-package luca.bigoni.mathematicalquiz.ui.home;
+package luca.bigoni.mathematicalquiz.ui.contatSupport;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class ContatSupportViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public ContatSupportViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is contat_support fragment");
     }
 
     public LiveData<String> getText() {
